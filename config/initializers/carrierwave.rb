b4fd574531974a943c4e7d2ae76c77987a12442d
Firehose <firehose@Marcos-Laptop.local>
2014-02-16 14:36:25 -0500

    Make it so we can upload photos and they can go on amazon s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  if Rails.env.production? # if it's alive
+    config.storage = :fog # use fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                         # required
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']                    # required
+  else
+    config.storage = :file
+  end
+end
